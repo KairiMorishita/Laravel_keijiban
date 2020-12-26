45c61f595a8dd2f229c0239788275bc09f1d3dd9
Kairi Morishita <k.moricchifc73@gmail.com>
2020-12-26 22:45:55 +0900

    Revert "Revert "first commit""
    This reverts commit 9f9a3cb8b59a67c5467f971eea191c5808ad0e25.

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\PostRequest;
+use Illuminate\Http\Request;
+use App\Post;
+use App\Photo;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\DB;
+
+class PostsController extends Controller
+{
+    /**
+     * 一覧表示
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $disk = Storage::disk('s3');
+        $files = $disk->files('/');
+        $posts = Post::orderBy('created_at', 'desc')->get();
+        return view('posts.index', ['posts' => $posts], ['files' => $files]);
+    }
+
+    /**
+     * 新規投稿処理
+     *
+     * @param \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //投稿する際のバリデーションを定義
+        $params = $request->validate([
+            'text' => 'required|max:50',
+            'user_id' => 'required|max:50',
+            'photo' => 'nullable|file|image|max:4000',
+        ]);
+
+        //投稿時に画像が添付されている場合はS3に画像を保存
+        if(array_key_exists("photo", $params)){
+                $file = $params['photo'];
+                $fileContents = file_get_contents($file->getRealPath());
+                $disk = Storage::disk('s3');
+                $disk->put($file->hashName(), $fileContents, 'public');
+                $params['filename'] = $file->hashName();
+                $params['image_path'] = Storage::disk('s3')->url($params['filename']);
+
+                $data =  Photo::create($params);
+                $params['photo_id'] = $data->id;
+        }
+        Post::create($params);
+        return redirect('/');
+    }
+
+    /**
+     * 投稿編集処理
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+	$request->validate([
+            'text' => 'required',
+        ]);
+        $posts = Post::findOrFail($id);
+        $posts->text = $request->text;
+        $posts->save();
+        return redirect('/');
+    }
+
+    /**
+    * 投稿削除処理
+    *
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
+    public function destroy($id)
+    {
+        $post = Post::findOrFail($id);
+        $post->delete();
+
+        if(isset($post->photo)){
+            $post->photo->delete();
+            $disk = Storage::disk('s3');
+            $disk->delete($post->photo->filename);
+        }
+
+        return redirect("/");
+    }
+
+}
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', '掲示板アプリ') }}</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- Custom CSS -->
+    <link href="{{ asset('css/style.min.css') }}" rel="stylesheet">
+    <!-- jquery -->
+    <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    <div id="app">
+        @yield('content')
+    </div>
+    <!-- Bootstrap js -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <!-- FontAwesome js -->
+    <script src="https://kit.fontawesome.com/6cbdd42b01.js" crossorigin="anonymous"></script>
+    <!-- Custom js -->
+    <script type="text/javascript" src="js/script.js"></script>
+</body>
+</html>
